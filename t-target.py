7ba92ddaa9fd124bb983dc9298bb7ca9a21ce770
Filipe_Freitas <113394371+Lip3e@users.noreply.github.com>
2024-03-05 15:23:25 -0300

    Update t-target.py

--- t-target.py
+++ t-target.py
@@ -1,10 +1,12 @@
+// Resposta da questão Cinco//
+
+
 def inverter_string(string):
     inverted_string = ''
     for i in range(len(string) - 1, -1, -1):
         inverted_string += string[i]
     return inverted_string
 
-# Exemplo de uso:
 entrada = input("Escreva uma string para inverter: ")
 resultado = inverter_string(entrada)
 print("String invertida:", resultado)
